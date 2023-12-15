628d86632b29e289dd75e10761ed1be1742fb7b6
KwonSeon <ksun2128@pukyong.ac.kr>
2023-12-16 00:13:53 +0900

    백준 2841. 외계인의 기타 연주 최적화

--- baekjoon/2841.py
+++ baekjoon/2841.py
@@ -1,25 +1,41 @@
 import sys
-from bisect import bisect_left, bisect_right
+# from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
 N, P = map(int, input().split())
+#
+# matrix = [[] for _ in range(N + 1)]
+# cnt = 0
+#
+# for i in range(N):
+# 	n, p = map(int, input().split())
+# 	position = bisect_left(matrix[n], p)
+#
+# 	if len(matrix[n]) == position:
+# 		matrix[n].append(p)
+# 		cnt += 1
+# 	else:
+# 		while matrix[n] and matrix[n][-1] > p:
+# 			matrix[n].pop()
+# 			cnt += 1
+# 		if not matrix[n] or matrix[n][-1] < p:
+# 			matrix[n].append(p)
+# 			cnt += 1
+# print(cnt)
 
-matrix = [[] for _ in range(N + 1)]
-cnt = 0
+stk =[[] for _ in range(7)]
+ans = 0
 
 for i in range(N):
-	n, p = map(int, input().split())
-	position = bisect_left(matrix[n], p)
+	string, p = map(int, input().split())
+	while stk[string] and stk[string][-1] > p:
+		stk[string].pop()
+		ans += 1
 
-	if len(matrix[n]) == position:
-		matrix[n].append(p)
-		cnt += 1
-	else:
-		while matrix[n] and matrix[n][-1] > p:
-			matrix[n].pop()
-			cnt += 1
-		if not matrix[n] or matrix[n][-1] < p:
-			matrix[n].append(p)
-			cnt += 1
-print(cnt)
+	if stk[string] and stk[string][-1] == p:
+		continue
+	stk[string].append(p)
+	ans += 1
+
+print(ans)
