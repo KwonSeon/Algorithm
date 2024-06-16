5b97ef43710f1aec9f5d5ee09efb3caf03fb1196
KwonSeon <ksun2128@pukyong.ac.kr>
2024-06-16 14:53:49 +0900

    백준 11652. 카드

--- baekjoon/1120.py
+++ baekjoon/1120.py
@@ -14,3 +14,27 @@ for i in range(diff + 1):
 	ans.append(count)
 
 print(min(ans))
+
+# a, b = map(str, input().split())
+#
+# # A와 B의 길이 차이를 계산
+# diff = len(b) - len(a)
+#
+# # 최소 차이를 저장할 변수
+# min_difference = float('inf')
+#
+# # 가능한 모든 위치에서 A를 B에 맞춰본다.
+# for i in range(diff + 1):
+# 	count = 0
+#
+# 	# A와 B의 현재 위치에서의 차이를 계산
+# 	for j in range(len(a)):
+# 		if a[j] != b[j + i]:
+# 			count += 1
+#
+# 	# 최소 차이를 갱신
+# 	if count < min_difference:
+# 		min_difference = count
+#
+# # 결과 출력
+# print(min_difference)
--- baekjoon/11652.py
+++ baekjoon/11652.py
@@ -1,29 +1,36 @@
+# import sys
+#
+# input = sys.stdin.readline
+#
+# n = int(input().strip())
+#
+# card_dic = {}
+#
+# for _ in range(n):
+# 	num = int(input().strip())
+# 	if num in card_dic:
+# 		card_dic[num] += 1
+# 	else:
+# 		card_dic[num] = 1
+#
+# sorted_card = sorted(card_dic.items(), key=lambda x:(-x[1], x[0]))
+#
+# print(sorted_card[0][0])
+
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
-N = int(input().strip())
+# 숫자 카드의 개수 입력
+n = int(input().strip())
 
-card_list = {}
+# 숫자 카드의 빈도를 저장할 Counter
+card_counter = Counter([int(input().strip()) for _ in range(n)])
 
-for _ in range(N):
-	num = int(input().strip())
-	if num in card_list:
-		card_list[num] += 1
-	else:
-		card_list[num] = 1
+# most_common 메서드로 빈도 계산 후 정렬
+common_card = card_counter.most_common()
+common_card.sort(key=lambda x: (-x[1], x[0]))
 
-
-sorted_card = sorted(card_list.items())
-
-print(sorted_card)
-
-for i in range(1, len(sorted_card)):
-	pre_num = sorted_card[i - 1][0]
-	pre_count = sorted_card[i - 1][1]
-
-	cur_num = sorted_card[i][0]
-	cur_count = sorted_card[i][1]
-
-	if pre_count != cur_count:
-		print(pre_num)
-		break
+# 가장 빈번한 숫자 출력
+print(common_card[0][0])
